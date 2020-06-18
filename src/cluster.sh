a3fc535e742b9dbca9295522b8a19d6960c869c9
Charles <charles.wilmot2@hotmail.fr>
2020-06-18 18:50:38 +0200

    dummy

--- src/cluster.sh
+++ src/cluster.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --mincpus 4
 #SBATCH --mem 4000
-#SBATCH --nodelist=turbine
+#SBATCH --exclude rockford
+
+##SBATCH --nodelist=turbine
 
 
 source $HOME/.software/python_environments/tensorflow_v2/bin/activate
