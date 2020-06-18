238263c6da6a9edb41f4be8a4a2845cebcf35986
Charles <charles.wilmot2@hotmail.fr>
2020-06-18 17:34:23 +0200

    enforce turbine

--- src/cluster.sh
+++ src/cluster.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --mincpus 4
 #SBATCH --mem 4000
+#SBATCH --nodelist=turbine
 
 
 source $HOME/.software/python_environments/tensorflow_v2/bin/activate
-echo "test"
-pip3 list
 srun -u python3 gym_experiment.py "$@"
