6ce579c739f28888aec4123536d7953ba8a993d6
Charles <charles.wilmot2@hotmail.fr>
2020-06-22 16:36:03 +0200

    new tuned conf file

--- src/environment.py
+++ src/environment.py
@@ -5,6 +5,11 @@ from gym import wrappers
 def get_environment(env_id, seed, monitor, outdir="./"):
     env = gym.make(env_id)
     if monitor:
-        env = wrappers.Monitor(env, directory=outdir, force=True)
+        env = wrappers.Monitor(
+            env,
+            directory=outdir,
+            force=True,
+            video_callable=lambda episode_id: episode_id % 250 == 0
+        )
     env.seed(seed)
     return env
