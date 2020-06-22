79131b4da051de6ff764e761abcdbbb7d6bffca6
Charles <charles.wilmot2@hotmail.fr>
2020-06-22 16:41:34 +0200

    try with xserver

--- src/cluster.sh
+++ src/cluster.sh
@@ -2,6 +2,8 @@
 #SBATCH --mincpus 4
 #SBATCH --mem 4000
 #SBATCH --exclude rockford,steele,hammer,conan,blomquist,wolfe,knatterton,holmes,lenssen,scuderi,matula,marlowe,poirot,monk
+#SBATCH -LXserver
+
 
 ##SBATCH --nodelist=turbine
 
