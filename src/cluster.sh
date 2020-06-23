4533a812e8c41f4f7828d6124c4b9aa257466136
Charles <charles.wilmot2@hotmail.fr>
2020-06-23 15:49:05 +0200

    fewer cpus

--- src/cluster.sh
+++ src/cluster.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --mincpus 4
+#SBATCH --mincpus 2
 #SBATCH --mem 4000
 #SBATCH --exclude rockford,steele,hammer,conan,blomquist,wolfe,knatterton,holmes,lenssen,scuderi,matula,marlowe,poirot,monk
 #SBATCH -LXserver
